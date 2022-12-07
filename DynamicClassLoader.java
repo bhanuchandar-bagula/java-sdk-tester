0ed1a0908269e465ced6c18d347aab5cdf4b6a2b
bhanuchandar-bagula <bhanuchandar.bagula@nutanix.com>
2022-12-07 14:01:26 +0530

    Update DynamicClassLoader.java

--- DynamicClassLoader.java
+++ DynamicClassLoader.java
@@ -1,6 +1,7 @@
 package org.example;
 
-import com.nutanix.vmm.java.client.ApiClient;
+import com.nutanix.dp1.vmmroot.vmm.v4.ahv.config.ClusterReference;
+import javafx.event.EventTarget;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
@@ -8,55 +9,82 @@ import java.io.*;
 import java.lang.reflect.Method;
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 
 public class DynamicClassLoader {
     public static void main(String[] args) throws Exception {
 
-        ApiClient client = new ApiClient();
-        client.setHost("10.46.153.34"); // IPv4/IPv6 address or FQDN of the cluster
-        client.setPort(9440); // Port to which to connect to
-        client.setUsername("admin"); // UserName to connect to the cluster
-        client.setPassword("Nutanix.123"); // Password to connect to the cluster}
-        client.setVerifySsl(false);
-
-//        Class<?> clientClass = Class.forName("com.nutanix.sto.java.client.ApiClient");
-//        Object client = clientClass.newInstance();
-//        Method setHost = clientClass.getDeclaredMethod("setHost", String.class);
-//        setHost.invoke(client, "10.46.153.34");
-//        Method setPort = clientClass.getDeclaredMethod("setPort", Integer.class);
-//        Integer port = (Integer) 9440;
-//        setPort.invoke(client, port);
-//        Method setUser = clientClass.getDeclaredMethod("setUsername", String.class);
-//        setUser.invoke(client, "admin");
-//        Method setPassword = clientClass.getDeclaredMethod("setPassword", String.class);
-//        setPassword.invoke(client, "Nutanix.123");
-//        Method setVerifySsl = clientClass.getDeclaredMethod("setVerifySsl", Boolean.class);
-//        setVerifySsl.invoke(client, false);
-
         String filePath = "C:\\Users\\bhanuchandar.bagula\\storage-sdk-2\\src\\main\\java\\org\\example\\sdk_config.json";
         HashMap[] arg1 = new HashMap[1];
+//        HashMap<String, Object> arg1 = new HashMap<>();
+//        arg1.put("extid", "uhnom");
 
         JSONParser parser = new JSONParser();
         try {
             Object obj = parser.parse(new InputStreamReader(new FileInputStream(filePath)));
             JSONObject jsonObject =  (JSONObject) obj;
 
-            JSONObject storageJson = (JSONObject) jsonObject.get("storage_sdk");
+            // API client object creation
+            JSONObject storageJson = (JSONObject) jsonObject.get("vmm_sdk");
+            String apiClientModule = (String) storageJson.get("api_client_module");
+            Class<?> clientClass = Class.forName(apiClientModule);
+            Object client = clientClass.newInstance();
+            System.out.println("API client instance crated");
+            Method setHost = clientClass.getDeclaredMethod("setHost", String.class);
+            System.out.println("Setting host");
+            setHost.invoke(client, "10.45.4.125");
+            Method setPort = clientClass.getDeclaredMethod("setPort", int.class);
+            int port = 9440;
+            System.out.println("Setting port");
+            setPort.invoke(client, port);
+            System.out.println("Setting username & password");
+            Method setUser = clientClass.getDeclaredMethod("setUsername", String.class);
+            setUser.invoke(client, "admin");
+            Method setPassword = clientClass.getDeclaredMethod("setPassword", String.class);
+            setPassword.invoke(client, "Nutanix.123");
+            Method setVerifySsl = clientClass.getDeclaredMethod("setVerifySsl", boolean.class);
+            setVerifySsl.invoke(client, false);
+
             String apiModule = (String) storageJson.get("api_module");
             String payloadModule = (String) storageJson.get("payload_module");
             String responseModule = (String) storageJson.get("response_module");
             String listResponseModule = (String) storageJson.get("list_response_module");
             String taskResponseModule = (String) storageJson.get("task_response_module");
+            String delResponseModule = (String) storageJson.get("delete_response_module");
+            String createResponseModule = (String) storageJson.get("create_response_module");
+            String updateResponseModule = (String) storageJson.get("update_response_module");
 
             Class<?> apiClass = Class.forName(apiModule);
-            Constructor<?> constructorApi = apiClass.getConstructor(ApiClient.class);
+            Constructor<?> constructorApi = apiClass.getConstructor(clientClass);
             Object apiObj = constructorApi.newInstance(client);
             Class<?> resClass = Class.forName(responseModule);
             Class<?> lisResClass = Class.forName(listResponseModule);
-            Class<?> taskResClass = Class.forName(taskResponseModule);
+            Class<?> delResClass = Class.forName(delResponseModule);
+            Class<?> createResClass = Class.forName(createResponseModule);
+            Class<?> updateResClass = Class.forName(updateResponseModule);
 
-            // get by extID
+            // list API call
+            System.out.println("Firing list API");
+            JSONObject listConfig = (JSONObject) storageJson.get("list");
+            String listMethod = (String) listConfig.get("method");
+            JSONArray listArgsType = (JSONArray) listConfig.get("arg_type");
+            JSONObject listArgs = (JSONObject) listConfig.get("args");
+            Class[] listParams = getArgTypeArray(listArgsType);
+            Integer page = ((Number) listArgs.get("page")).intValue();
+            Integer limit = ((Number) listArgs.get("limit")).intValue();
+            String filter = (String) listArgs.get("filter");
+            String order_by = (String) listArgs.get("order_by");
+//            String select = (String) listArgs.get("select");
+            Method getAllStorageContainers = apiClass.getDeclaredMethod(listMethod, listParams);
+            Object list_response = lisResClass.cast(getAllStorageContainers.invoke
+                    (apiObj,page,limit,filter,order_by,arg1));
+            Method listData = lisResClass.getDeclaredMethod("getData");
+            Object listDataResponse = listData.invoke(list_response);
+            System.out.println("List Response : " + listDataResponse);
+
+            // get API call
+            System.out.println("Firing GET API");
             JSONObject getConfig = (JSONObject) storageJson.get("get");
             String getMethod = (String) getConfig.get("method");
             JSONArray getArgsType = (JSONArray) getConfig.get("arg_type");
@@ -67,27 +95,15 @@ public class DynamicClassLoader {
             Object extIdResponse = resClass.cast(getStorageContainerByExtId.invoke(apiObj, ext_id, arg1));
             Method getData = resClass.getDeclaredMethod("getData");
             Object getDataResponse = getData.invoke(extIdResponse);
-            System.out.println(" : " + getDataResponse);
+            System.out.println("Get Response : " + getDataResponse);
+            Method getReseved = resClass.getDeclaredMethod("get$reserved");
+            Object getReservedResponse = getReseved.invoke(extIdResponse);
+            int first = getReservedResponse.toString().indexOf("ETag=") + 5;
+            int last = getReservedResponse.toString().length() - 1;
+            String Etag = getReservedResponse.toString().substring(first, last);
+            System.out.println("Etag : " + Etag);
 
-            // list
-            JSONObject listConfig = (JSONObject) storageJson.get("list");
-            String listMethod = (String) listConfig.get("method");
-            JSONArray listArgsType = (JSONArray) listConfig.get("arg_type");
-            JSONObject listArgs = (JSONObject) listConfig.get("args");
-            Class[] listParams = getArgTypeArray(listArgsType);
-            Integer page = ((Number) listArgs.get("page")).intValue();
-            Integer limit = ((Number) listArgs.get("limit")).intValue();
-            String filter = (String) listArgs.get("filter");
-            String order_by = (String) listArgs.get("order_by");
-            String select = (String) listArgs.get("select");
-            Method getAllStorageContainers = apiClass.getDeclaredMethod(listMethod, listParams);
-            Object list_response = lisResClass.cast(getAllStorageContainers.invoke
-                    (apiObj,page,limit,filter,order_by,select,arg1));
-            Method listData = lisResClass.getDeclaredMethod("getData");
-            Object listDataResponse = listData.invoke(list_response);
-            System.out.println(" : " + listDataResponse);
-
-            // add storage container
+            // create API call
             JSONObject createConfig = (JSONObject) storageJson.get("create");
             String createMethod = (String) createConfig.get("method");
             JSONObject createPayload = (JSONObject) createConfig.get("payload");
@@ -102,36 +118,79 @@ public class DynamicClassLoader {
                 String key = (String) o;
                 if (createPayloadArgType.get(key).equals("int")){
                     Integer arg = ((Number) createPayload.get(key)).intValue();
-                    System.out.println(" : " + arg);
                     Method set = payloadClass.getDeclaredMethod(key, Integer.class);
                     set.invoke(payloadObj, arg);
+                }
+                else if (createPayloadArgType.get(key).equals("cluster_reference")) {
+                    String cluster_id = (String) createPayload.get(key);
+                    ClusterReference clusterReference = new ClusterReference();
+                    clusterReference.setExtId(cluster_id);
+                    Method set = payloadClass.getDeclaredMethod(key, clusterReference.getClass());
+                    set.invoke(payloadObj, clusterReference);
                 }
                 else{
                 Method set = payloadClass.getDeclaredMethod(key, createPayload.get(key).getClass());
                 set.invoke(payloadObj, createPayload.get(key));
                 }
             }
-            String xClusterId = (String) createArgs.get("x_cluster_id");
+//            String xClusterId = (String) createArgs.get("x_cluster_id");
             Method addStorageContainerForCluster = apiClass.getDeclaredMethod(createMethod, createParams);
-            Object add_response = taskResClass.cast(addStorageContainerForCluster.invoke(apiObj, payloadObj, xClusterId, arg1));
-            Method addData = taskResClass.getDeclaredMethod("getData");
+            System.out.println("Firing Create API");
+            Object add_response = createResClass.cast(addStorageContainerForCluster.invoke(apiObj, payloadObj, arg1));
+            Method addData = createResClass.getDeclaredMethod("getData");
             Object addDataResponse = addData.invoke(add_response);
-            System.out.println(" : " + addDataResponse);
+            System.out.println("Create Response : " + addDataResponse);
 
-            // delete storage container
+            // update API call
+            JSONObject updateConfig = (JSONObject) storageJson.get("update");
+            String updateMethod = (String) updateConfig.get("method");
+            JSONObject updatePayload = (JSONObject) updateConfig.get("payload");
+            JSONObject updatePayloadArgType = (JSONObject) updateConfig.get("payload_arg_type");
+            JSONArray updateArgsType = (JSONArray) updateConfig.get("arg_type");
+            Class[] updateParams = getArgTypeArray(updateArgsType);
+            JSONObject updateArgs = (JSONObject) updateConfig.get("args");
+            Class<?> payloadClassUpdate = Class.forName(payloadModule);
+            Object payloadObjUpdate = payloadClassUpdate.newInstance();
+            updateParams[0] = payloadClassUpdate;
+            for (Object o : updatePayload.keySet()) {
+                String key = (String) o;
+                if (updatePayloadArgType.get(key).equals("int")){
+                    Integer arg = ((Number) updatePayload.get(key)).intValue();
+                    Method set = payloadClassUpdate.getDeclaredMethod(key, Integer.class);
+                    set.invoke(payloadObjUpdate, arg);
+                }
+                else{
+                    Method set = payloadClassUpdate.getDeclaredMethod(key, updatePayload.get(key).getClass());
+                    set.invoke(payloadObjUpdate, updatePayload.get(key));
+                }
+            }
+            String containerIdUpdate = (String) updateArgs.get("ext_id");
+            Method updateStorageContainerForCluster = apiClass.getDeclaredMethod(updateMethod, updateParams);
+            System.out.println("Firing Update API");
+            arg1[0] = new HashMap<String, Object>();
+            arg1[0].put("If-Match", Etag);
+            Object update_response = updateResClass.cast(updateStorageContainerForCluster.invoke(apiObj, payloadObjUpdate, containerIdUpdate, arg1));
+            Method updateData = updateResClass.getDeclaredMethod("getData");
+            Object updateDataResponse = updateData.invoke(update_response);
+            System.out.println("Update Response : " + updateDataResponse);
+
+            // delete API call
             JSONObject delConfig = (JSONObject) storageJson.get("delete");
             String delMethod = (String) delConfig.get("method");
             JSONArray delArgsType = (JSONArray) delConfig.get("arg_type");
             JSONObject delArgs = (JSONObject) delConfig.get("args");
             Class[] delParams = getArgTypeArray(delArgsType);
             String delContainerId = (String) delArgs.get("container_id");
-            Boolean ignoreSmallFiles = (Boolean) delArgs.get("ignore_small_file");
+//            Boolean ignoreSmallFiles = (Boolean) delArgs.get("ignore_small_file");
             Method deleteStorageContainerByExtId = apiClass.getDeclaredMethod(delMethod, delParams);
-            Object del_response = taskResClass.cast(deleteStorageContainerByExtId.invoke(apiObj,
-                    delContainerId, ignoreSmallFiles, arg1));
-            Method delData = taskResClass.getDeclaredMethod("getData");
+            System.out.println("Firing Delete API");
+            arg1[0] = new HashMap<String, Object>();
+            arg1[0].put("If-Match", Etag);
+            Object del_response = delResClass.cast(deleteStorageContainerByExtId.invoke(apiObj,
+                    delContainerId, arg1));
+            Method delData = delResClass.getDeclaredMethod("getData");
             Object delDataResponse = delData.invoke(del_response);
-            System.out.println(" : " + delDataResponse);
+            System.out.println("Delete Response : " + delDataResponse);
 
 
         } catch (ParseException e) {
